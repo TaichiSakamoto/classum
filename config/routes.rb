0bf363a92024f7eec0add4aa6b507a6f8ff80a0b
TaichiSakmoto <sakamo.tk76@gmail.com>
2018-04-02 14:01:24 +0900

    LogIn機能実装

---
+++ config/routes.rb
@@ -0,0 +1,40 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'users/new'
+
+  get 'pages/index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'pages#index'
+end
+
+Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'users/new'
+
+  root 'pages#index'
+  get 'pages/help'
+end
+
+Rails.application.routes.draw do
+  get 'sessions/new'
+
+  root 'pages#index'
+
+  get 'pages/help'
+  resources 'users'
+end
+
+Rails.application.routes.draw do
+  root 'pages#index'
+
+  get 'pages/help'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  resources 'users'
+end
